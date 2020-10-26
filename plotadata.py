29a5da12c3d05df721b1005440ab8ff9eefa35e0
Andrea <andrea.capra85@gmail.com>
2020-10-26 10:28:32 -0700

    clean-up

--- plotadata.py
+++ plotadata.py
@@ -23,7 +23,7 @@ var1=['totale_positivi','totale_ospedalizzati','terapia_intensiva','deceduti']
 
 fig=plt.figure(figsize=(15,8))
 plt.title('COVID-19 Italia - Monitoraggio situazione')
-#plt.subplot(211)
+
 
 for x in var1:
     y=np.array([d[x] for d in l])
@@ -32,30 +32,7 @@ for x in var1:
 plt.legend(loc='upper left',fontsize=23)
 plt.xticks(xlab,fontsize=10)
 plt.yticks(fontsize=16)
-#plt.ticklabel_format(axis='y', style='sci', scilimits=(0,0),useMathText=True)
 plt.grid()
-
-'''
-plt.subplot(212)
-var2=['nuovi_attualmente_positivi']
-for x in var2:
-    y=np.array([d[x] for d in l])
-    plt.plot(t,y,label=x)
-
-print(l[0]['tamponi'],l[1]['tamponi']-l[0]['tamponi'])
-ytemp = [None] * Ndays
-ytemp[0]=l[0]['tamponi']
-for i in range(1,Ndays):
-    ytemp[i]=l[i]['tamponi']-l[i-1]['tamponi']
-ytamp=np.array(ytemp)
-plt.plot(t,ytamp,label='nuovi tamponi')
-
-    
-plt.legend(loc='upper left',fontsize=23)
-plt.xticks(xlab,fontsize=16)
-plt.yticks(fontsize=16)
-plt.grid()
-'''
 
 plt.tight_layout()
 
